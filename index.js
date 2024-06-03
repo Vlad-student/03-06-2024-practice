b7e2de25359c1d673794d66c57f8a888b750bebd
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-06-03 19:01:45 +0300

    прорроро

--- index.js
+++ index.js
@@ -107,7 +107,37 @@ console.group('5 task')
 console.groupEnd();
 
 console.group('6 task')
-
+// monthNumber = `Пори року:
+// 1 ,2, 12  зима
+// 3, 4, 5  весна
+// 6, 7,8 літо
+// 9, 10,11  осінь
+// `
+// const month = prompt (monthNumber)
+// switch (month) {
+//     case '1':
+//     case '2':
+//     case '12':
+//      alert("Зима");
+//       break;
+//     case '3':
+//     case '4':
+//     case '5':
+//       alert("Весна");
+//       break;
+//     case '6':
+//     case '7':
+//     case '8':
+//       alert("Літо");
+//       break;
+//     case '9':
+//     case '10':
+//     case '11':
+//       alert("Осінь");
+//       break;
+//     default:
+//     alert("Невірний місяць");
+//       break;}
 
 console.groupEnd();
 
