a76918ff4b3ad6f7ce457f81f459e286b08b2643
Vlad-student <vlad.v.terletskiy@gmail.com>
2024-06-03 18:09:06 +0300

    practice-03-06-2024

---
+++ index.js
@@ -0,0 +1,47 @@
+console.group('1 task')
+// console.log('1 Напишіть програму, яка перевіряє, чи є користувач повнолітнім, і виводить відповідне повідомлення.');
+// let userAge = prompt('Enter your age');
+// const number= Number(userAge);
+// let result = number >= 18 ? 'Повнолітній' : 'Не повнолітній' ;
+// console.log(result);
+console.groupEnd();
+
+console.group('2 task')
+// let userAge = prompt('Enter your age');
+// const number= Number(userAge);
+
+//  if (number<0) {
+//     alert('не визначено');
+// }else if (number<12) {
+//     alert('child');
+// }else if(number <18){
+//     alert('teen');
+// }else if(number<=65){
+//     alert('adult');
+// }else if(number>65){
+//     alert('elderly person')
+// }
+console.groupEnd();
+
+console.group('3 task')
+// let userHour = prompt('Enter time');
+// const hour= Number(userHour);
+// if (hour>= 0 && hour <5 || hour>=22 && hour <24) {
+//      alert("Доброї ночі!");
+// }else if (hour >= 6 && hour < 11) {
+//     alert("Доброго ранку!");
+//   } else if (hour >= 11 && hour < 18) {
+//     alert("Доброго дня!");
+//   } else if (hour >= 18 && hour < 20) {
+//     alert("Доброго вечора!");
+//   }
+console.groupEnd();
+
+console.group('4 task')
+
+
+console.groupEnd();
+
+
+
+
